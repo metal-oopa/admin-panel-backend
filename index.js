3a0806eee3614acba26ab78a79f7496a24c8b201
metal-oopa <sudip5banerjee1974@gmail.com>
2022-07-23 12:50:30 +0530

    add cors origin

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://hirable-frontend.herokuapp.com/",
     credentials: true,
   })
 );
